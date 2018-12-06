b4ed872a4c8b7d4ab0d11c8f0686263a9efa1cec
vuletiki <vu.le@tiki.vn>
2018-12-06 10:15:59 +0700

    1

--- index.js
+++ index.js
@@ -3,54 +3,36 @@ const puppeteer = require('puppeteer');
 var express = require('express')
 var fs = require('fs')
 var ReportGenerator = require('./node_modules/lighthouse/lighthouse-core/report/report-generator');
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+	return chromeLauncher
+		.launch({ chromeFlags: opts.chromeFlags })
+		.then(chrome => {
+			opts.port = chrome.port;
+			return lighthouse(url, opts, config).then(results => {
+				return chrome.kill().then(() => results.lhr);
+			});
+		})
+		.catch(err => {
+			console.log(err)
+		});
+}
+
 var app = express()
-// fetch('https://hooks.slack.com/services/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', {
-//     method: 'post',
-//     body: JSON.stringify({
-//     "attachments": [
-//         {
-//             "fallback": "Required plain-text summary of the attachment.",
-//             "color": "#2eb886",
-//             "pretext": "Page content has been change",
-//             "title": "tiki.vn",
-//             "title_link": "https://tiki.vn"
-//         }
-//     ]
-// }),
-//     headers: { 'Content-Type': 'application/json' },
-// })
 
 app.use(express.static('public'))
-app.get('/sc', (req, res) => {
-	screenshoot('https://tikilighthouse.herokuapp.com/1544063777970.572.html', '1544063777970.572')
-})
-app.get('/take', function (req, res) {
-	const lighthouse = require("lighthouse");
-	const chromeLauncher = require("chrome-launcher");
 
-	var file = (new Date()).getTime() + Math.random()
-	function launchChromeAndRunLighthouse(url, opts, config = null) {
-		return chromeLauncher
-			.launch({ chromeFlags: opts.chromeFlags })
-			.then(chrome => {
-				opts.port = chrome.port;
-				return lighthouse(url, opts, config).then(results => {
-					return chrome.kill().then(() => results.lhr);
-				});
-			})
-			.catch(err => {
-				console.log(err)
-			});
-	}
-
+app.get('/lookup', async function (req, res) {
 	const opts = {
-		// chromeFlags: ["--show-paint-rects"]
 		outputPath: './report.html'
 	};
+	var file = (new Date()).getTime() + Math.random()
+	var htmlFile = file + ".html";
+	var pngFile = file + ".png";
 	launchChromeAndRunLighthouse(req.query.url, opts)
 	.then(data => {
-		var htmlFile = file + ".html";
-		var pngFile = file + ".png";
 		console.log(htmlFile)
 		const html = ReportGenerator.generateReportHtml(data);
 		fs.writeFile("public/" + htmlFile, html, async function(err) {
@@ -59,19 +41,17 @@ app.get('/take', function (req, res) {
 			const page = await browser.newPage();
 			await page.goto('https://tikilighthouse.herokuapp.com/' + htmlFile);
 			await page.screenshot({
-				// fullPage: true,
 				path:'./public/' + pngFile,
-				clip: {x: 228, y: 134, width: 1046 - 228, height: 286 - 134}
+				clip: {x: 0, y: 134, width: 800, height: 160}
 			});
 			fetch('https://hooks.slack.com/services/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', {
 			    method: 'post',
 			    body: JSON.stringify({
 				    "attachments": [
 				        {
-				            "fallback": "Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event",
-				            "title": "Network traffic (kb/s)",
-				            "title_link": "https://datadog.com/path/to/event",
-				            "text": "How does this look? @slack-ops - Sent by Julie Dodd",
+				            "title": "lighthouse Report",
+				            "title_link": 'https://tikilighthouse.herokuapp.com/' + htmlFile,
+				            "text": `Page: ${req.query.url}`,
 				            "image_url": "https://tikilighthouse.herokuapp.com/" + pngFile,
 				            "color": "#764FA5"
 				        }
@@ -80,14 +60,13 @@ app.get('/take', function (req, res) {
 			    headers: { 'Content-Type': 'application/json' },
 			})
 			await browser.close();
-			// take.fromURL('https://tikilighthouse.herokuapp.com/' + htmlFile, './public/' + pngFile, {clip: {x: 228, y: 134, width: 1046 - 228, height: 286 - 134},waitMilliseconds: 5000}, function(a, b){
-			// 	console.log(a, b)
-			// 	//an image of google.com has been saved at ./test.png
-			// });
 		});
 	})
-	res.send(String(file))
+	res.json({
+		htmlFile,
+		pngFile
+	})
 	// .then(renderTable);
 
 })
-app.listen(process.env.PORT || 8080); //the server object listens on port 8080
+app.listen(process.env.PORT || 8080);
