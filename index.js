573acaffd5cea7cad565a723e53354a4cdc752cf
vuletiki <vu.le@tiki.vn>
2018-12-06 09:49:11 +0700

    1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch');
+const puppeteer = require('puppeteer');
 var express = require('express')
 var fs = require('fs')
 var ReportGenerator = require('./node_modules/lighthouse/lighthouse-core/report/report-generator');
 var app = express()
-var take = require("node-server-screenshot");
 // fetch('https://hooks.slack.com/services/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', {
 //     method: 'post',
 //     body: JSON.stringify({
@@ -21,7 +21,9 @@ var take = require("node-server-screenshot");
 // })
 
 app.use(express.static('public'))
-
+app.get('/sc', (req, res) => {
+	screenshoot('https://tikilighthouse.herokuapp.com/1544063777970.572.html', '1544063777970.572')
+})
 app.get('/take', function (req, res) {
 	const lighthouse = require("lighthouse");
 	const chromeLauncher = require("chrome-launcher");
@@ -51,28 +53,37 @@ app.get('/take', function (req, res) {
 		var pngFile = file + ".png";
 		console.log(htmlFile)
 		const html = ReportGenerator.generateReportHtml(data);
-		fs.writeFile("public/" + htmlFile, html, function(err) {
+		fs.writeFile("public/" + htmlFile, html, async function(err) {
 			console.log('save ok')
-			take.fromURL('https://tikilighthouse.herokuapp.com/' + htmlFile, './public/' + pngFile, {clip: {x: 228, y: 134, width: 1046 - 228, height: 286 - 134},waitMilliseconds: 5000}, function(a, b){
-				console.log(a, b)
-				//an image of google.com has been saved at ./test.png
-				fetch('https://hooks.slack.com/services/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', {
-				    method: 'post',
-				    body: JSON.stringify({
-					    "attachments": [
-					        {
-					            "fallback": "Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event",
-					            "title": "Network traffic (kb/s)",
-					            "title_link": "https://datadog.com/path/to/event",
-					            "text": "How does this look? @slack-ops - Sent by Julie Dodd",
-					            "image_url": "https://tikilighthouse.herokuapp.com/" + pngFile,
-					            "color": "#764FA5"
-					        }
-					    ]
-					}),
-				    headers: { 'Content-Type': 'application/json' },
-				})
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+			await page.goto('https://tikilighthouse.herokuapp.com/' + htmlFile);
+			await page.screenshot({
+				// fullPage: true,
+				path:'./public/' + pngFile,
+				clip: {x: 228, y: 134, width: 1046 - 228, height: 286 - 134}
 			});
+			fetch('https://hooks.slack.com/services/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', {
+			    method: 'post',
+			    body: JSON.stringify({
+				    "attachments": [
+				        {
+				            "fallback": "Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event",
+				            "title": "Network traffic (kb/s)",
+				            "title_link": "https://datadog.com/path/to/event",
+				            "text": "How does this look? @slack-ops - Sent by Julie Dodd",
+				            "image_url": "https://tikilighthouse.herokuapp.com/" + pngFile,
+				            "color": "#764FA5"
+				        }
+				    ]
+				}),
+			    headers: { 'Content-Type': 'application/json' },
+			})
+			await browser.close();
+			// take.fromURL('https://tikilighthouse.herokuapp.com/' + htmlFile, './public/' + pngFile, {clip: {x: 228, y: 134, width: 1046 - 228, height: 286 - 134},waitMilliseconds: 5000}, function(a, b){
+			// 	console.log(a, b)
+			// 	//an image of google.com has been saved at ./test.png
+			// });
 		});
 	})
 	res.send(String(file))
